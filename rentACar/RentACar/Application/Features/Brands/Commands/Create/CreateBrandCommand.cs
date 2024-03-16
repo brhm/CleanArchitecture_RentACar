7c11c23c7de534d10442cbf23e96cac765bd82e2
ibrahim keskin <brhmkskn@outlook.com>
2024-03-16 22:54:37 +0100

    İmplementing Global Transaction

--- rentACar/RentACar/Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
+++ rentACar/RentACar/Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Features.Brands.Rules;
 using Application.Services.Repositories;
 using AutoMapper;
+using Core.Application.Pipelines.Transaction;
 using Domain.Entities;
 using MediatR;
 using System;
@@ -11,7 +12,7 @@ using System.Threading.Tasks;
 
 namespace Application.Features.Brands.Commands.Create;
 
-public class CreateBrandCommand:IRequest<CreatedBrandResponse>
+public class CreateBrandCommand:IRequest<CreatedBrandResponse>,ITransactionalRequest
 {
     public string Name { get; set; }
 
@@ -34,6 +35,7 @@ public class CreateBrandCommand:IRequest<CreatedBrandResponse>
             brand.Id = Guid.NewGuid();
 
             await _brandRepository.AddAsync(brand);
+            //await _brandRepository.AddAsync(brand); // Test Transaction codes.  
 
             CreatedBrandResponse createdBrandResponse=_mapper.Map<CreatedBrandResponse>(brand);
 
