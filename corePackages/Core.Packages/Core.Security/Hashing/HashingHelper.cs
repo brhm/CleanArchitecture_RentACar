21ee82ccc0fc9d9ac26ca205658d6d6a292de3c2
ibrahim keskin <brhmkskn@outlook.com>
2024-03-20 14:44:34 +0100

    Added Security Models and Configurations

---
+++ corePackages/Core.Packages/Core.Security/Hashing/HashingHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Security.Hashing;
+
+public static class HashingHelper
+{
+    public static void CreatePasswordHash(string password,out byte[] passwordHash,out byte[] passwordSalt)
+    {
+        using HMACSHA512 hmac=new HMACSHA512();
+
+        passwordHash=hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+        passwordSalt = hmac.Key;
+    }
+    public static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+    {
+        using HMACSHA512 hmac=new HMACSHA512(passwordSalt);
+
+        byte[] computedHash=hmac.ComputeHash(Encoding.UTF8.GetBytes((string)password));
+
+        return computedHash.SequenceEqual(passwordHash);
+    }
+}
